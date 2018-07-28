01e44420b3fe4f5dd30e36b6b7f35312f5bb0be2
郭方超 <guofangchao@daling.com>
2018-07-28 21:18:42 +0800

    add utils

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const main = require("./lib/main");
 module.exports = {
     Controller: main.Controller,
     Schedule: main.Schedule,
+    config: main.config,
+    utils: main.utils,
+    Logger: main.Logger
 }
 require("./lib/index");
